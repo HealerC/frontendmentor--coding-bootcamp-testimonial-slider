3091b39547d64b0ef80cb216438b4e5d90d4ccff
Uyi <ihazauyi@gmail.com>
2022-08-01 12:56:56 +0100

    feat: Initialize component elements with placeholder strings

--- src/components/ReviewPicture.js
+++ src/components/ReviewPicture.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const ReviewPicture = () => {
-  return <div>ReviewPicture</div>;
+  return (
+    <section className="card__picture">
+      <img
+        src="picture-backImage"
+        alt="Picture backimage"
+        className="card__backImage"
+      />
+      <div className="card__photoContainer">
+        <div className="card__photoDiv"></div>
+        <section className="card__changeReviews">{"< >"}</section>
+      </div>
+    </section>
+  );
 };
 
 export default ReviewPicture;
--- src/components/ReviewText.js
+++ src/components/ReviewText.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const ReviewText = () => {
   return (
-    <div>ReviewText</div>
-  )
-}
+    <article className="card__text">
+      <h1 className="card__testimonial">
+        “ I’ve been interested in coding for a while but never taken the jump,
+        until now. I couldn’t recommend this course enough. I’m now in the job
+        of my dreams and so excited about the future. ”
+      </h1>
+      <section className="card__bio">
+        <p className="card__name">Tanya Sinclair</p>
+        <p className="card__role">UX Engineer</p>
+      </section>
+    </article>
+  );
+};
 
-export default ReviewText
+export default ReviewText;
