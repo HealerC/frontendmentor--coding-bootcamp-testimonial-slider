faf58e12f36a648f6adf41558434662afd400cd6
Uyi <ihazauyi@gmail.com>
2022-08-01 23:54:25 +0100

    feat: Centering content on page (+others maybe)

--- src/components/ReviewPicture.js
+++ src/components/ReviewPicture.js
@@ -3,15 +3,12 @@ import patternBackground from "../images/pattern-bg.svg";
 const ReviewPicture = ({ name, url, prev, next }) => {
   return (
     <section className="card__picture">
-      <img src={patternBackground} alt="" className="card__backgroundImage" />
+      {/* <img src={patternBackground} alt="" className="card__backgroundImage" /> */}
       <div className="card__photoContainer">
         <div
           className="card__photoDiv"
           style={{
             backgroundImage: `url(${url})`,
-            width: "500px",
-            height: "400px",
-            border: "2px solid gray",
           }}
         >
           <span className="sr-only">Picture of {name}</span>
