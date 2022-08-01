033546ddb7e855166aa2ff443ae17c4d6c9d39e8
Uyi <ihazauyi@gmail.com>
2022-08-02 00:40:41 +0100

    feat: Position the review texts

--- src/components/ReviewText.js
+++ src/components/ReviewText.js
@@ -3,11 +3,13 @@ import React from "react";
 const ReviewText = ({ name, role, review }) => {
   return (
     <article className="card__text">
-      <h1 className="card__testimonial">{review || "Hello world"}</h1>
-      <section className="card__bio">
-        <p className="card__name">{name || "John Smith"}</p>
-        <p className="card__role">{role || "POTUS"}</p>
-      </section>
+      <div className="text-container">
+        <h1 className="card__testimonial">{review || "Hello world"}</h1>
+        <section className="card__bio">
+          <p className="card__name">{name || "John Smith"}</p>
+          <p className="card__role">{role || "POTUS"}</p>
+        </section>
+      </div>
     </article>
   );
 };
