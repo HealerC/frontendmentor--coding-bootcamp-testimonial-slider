2500cb392cce93c1ca17281072703036474213e3
Uyi <ihazauyi@gmail.com>
2022-08-02 22:28:50 +0100

    feat: Navigate the reviews on user keyboard presses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./stylesheets/App.css";
 import { ReviewText, ReviewPicture, ExtraActions } from "./components";
 import list from "./placeholder-data";
@@ -7,15 +7,37 @@ function App() {
   const [reviewData, setReviewData] = useState(list);
   const [activeReview, setActiveReview] = useState(0);
 
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  });
   const prevReview = () => {
+    // first review
     if (activeReview === 0) return setActiveReview(reviewData.length - 1);
     setActiveReview(activeReview - 1);
   };
   const nextReview = () => {
-    if (activeReview === reviewData.length - 1)
-      // last review
-      return setActiveReview(0);
+    // last review
+    if (activeReview === reviewData.length - 1) return setActiveReview(0);
     setActiveReview(activeReview + 1);
+  };
+  const handleKeyPress = (event) => {
+    const key = event.key;
+    switch (key) {
+      case "ArrowLeft":
+      case "P":
+      case "p":
+        prevReview();
+        break;
+      case "ArrowRight":
+      case "N":
+      case "n":
+        nextReview();
+        break;
+      default:
+    }
   };
   return (
     <main className="card">
