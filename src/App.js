6b4bc28e5d092dbcda2f889206a98c3df0a7a790
Uyi <ihazauyi@gmail.com>
2022-08-03 16:51:48 +0100

    feat: Initialize add, edit and delete functionality, also implemented delete

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { ReviewText, ReviewPicture, ExtraActions } from "./components";
 import list from "./placeholder-data";
 
 function App() {
-  const [reviewData, setReviewData] = useState(list);
-  const [activeReview, setActiveReview] = useState(0);
+  const [reviewData, setReviewData] = useState({ list, active: 0 });
+  // const [activeReview, setActiveReview] = useState(0);
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeyPress);
@@ -13,15 +13,19 @@ function App() {
       document.removeEventListener("keydown", handleKeyPress);
     };
   });
+
   const prevReview = () => {
-    // first review
-    if (activeReview === 0) return setActiveReview(reviewData.length - 1);
-    setActiveReview(activeReview - 1);
+    if (reviewData.active === 0)
+      return setReviewData({
+        ...reviewData,
+        active: reviewData.list.length - 1,
+      });
+    setReviewData({ ...reviewData, active: reviewData.active - 1 });
   };
   const nextReview = () => {
-    // last review
-    if (activeReview === reviewData.length - 1) return setActiveReview(0);
-    setActiveReview(activeReview + 1);
+    if (reviewData.active === reviewData.list.length - 1)
+      return setReviewData({ ...reviewData, active: 0 });
+    setReviewData({ ...reviewData, active: reviewData.active + 1 });
   };
   const handleKeyPress = (event) => {
     const key = event.key;
@@ -39,20 +43,47 @@ function App() {
       default:
     }
   };
+  const deleteReview = () => {
+    setReviewData((state) => {
+      const newList = state.list.filter(
+        (data, index) => index !== state.active
+      );
+      let newActive = state.active - 1;
+      if (newActive < 0) {
+        newActive = newList.length - 1;
+      }
+      return { list: newList, active: newActive };
+    });
+  };
+  const addReview = () => {
+    console.log("Add review");
+  };
+  const editReview = () => {
+    console.log("Edit " + activeReview);
+  };
+
+  const activeReview = reviewData.list[reviewData.active];
   return (
     <main className="card">
-      <ReviewText
-        name={reviewData[activeReview].name}
-        role={reviewData[activeReview].role}
-        review={reviewData[activeReview].review}
-      />
-      <ReviewPicture
-        name={reviewData[activeReview].name}
-        url={reviewData[activeReview].photoUrl}
-        prev={prevReview}
-        next={nextReview}
-      />
-      <ExtraActions />
+      {!activeReview ? (
+        <h1>Nothing to show</h1>
+      ) : (
+        <>
+          <ReviewText
+            name={activeReview.name}
+            role={activeReview.role}
+            review={activeReview.review}
+          />
+          <ReviewPicture
+            name={activeReview.name}
+            url={activeReview.photoUrl}
+            prev={prevReview}
+            next={nextReview}
+          />
+        </>
+      )}
+
+      <ExtraActions add={addReview} del={deleteReview} edit={editReview} />
     </main>
   );
 }
--- src/components/ExtraActions.js
+++ src/components/ExtraActions.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const ExtraActions = () => {
-  return <div className="card__extraActions">ExtraActions</div>;
+const ExtraActions = ({ edit, add, del }) => {
+  return (
+    <aside className="card__extraActions">
+      <span onClick={edit}>E</span>
+      <span onClick={add}>A</span>
+      <span onClick={del}>D</span>
+    </aside>
+  );
 };
 
 export default ExtraActions;
