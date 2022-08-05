057504271eed5de9a848c9bf4b8c729d825bf624
Uyi <ihazauyi@gmail.com>
2022-08-05 18:03:03 +0100

    feat: Change the view of the picture when user is editing/adding a review

--- src/components/ReviewPicture.js
+++ src/components/ReviewPicture.js
@@ -1,26 +1,43 @@
 import prevIcon from "../images/icon-prev.svg";
 import nextIcon from "../images/icon-next.svg";
+import { FaImage } from "react-icons/fa";
 
-const ReviewPicture = ({ name, url, prev, next }) => {
+const ReviewPicture = ({ name, url, prev, next, edit }) => {
   return (
     <section className="card__picture">
       {/* <img src={patternBackground} alt="" className="card__backgroundImage" /> */}
       <div className="card__photoContainer">
         <div
           className="card__photoDiv"
-          style={{
-            backgroundImage: `url(${url})`,
-          }}
+          style={
+            edit.isEditing || edit.isAdding
+              ? {
+                  backgroundImage: `url(${edit.photoUrl})`,
+                }
+              : {
+                  backgroundImage: `url(${url})`,
+                }
+          }
         >
-          <span className="sr-only">Picture of {name}</span>
-          <section className="card__changeReviews">
-            <button onClick={prev}>
-              <img src={prevIcon} alt="Previous review icon" />
-            </button>
-            <button onClick={next}>
-              <img src={nextIcon} alt="Next review icon" />
-            </button>
-          </section>
+          {(edit.isEditing || edit.isAdding) && (
+            <div className="card__changePhoto">
+              <FaImage className="icon-changePhoto" />
+              <p>Change photo</p>
+            </div>
+          )}
+          {!edit.isEditing && !edit.isAdding && (
+            <>
+              <span className="sr-only">Picture of {name}</span>
+              <section className="card__changeReviews">
+                <button onClick={prev}>
+                  <img src={prevIcon} alt="Previous review icon" />
+                </button>
+                <button onClick={next}>
+                  <img src={nextIcon} alt="Next review icon" />
+                </button>
+              </section>
+            </>
+          )}
         </div>
       </div>
     </section>
